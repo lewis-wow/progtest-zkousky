fff3b11f170ddc50fcaaa26f843be60bb3b88587
Ludvík Prokopec <ludvik.prokopec@gmail.com>
2021-12-26 19:51:18 +0100

    added link

--- mzdy/index.c
+++ mzdy/index.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    UKOL:
+    https://www.fit-wiki.cz/%C5%A1kola/p%C5%99edm%C4%9Bty/bi-pa1/pa1_zkouska_2018_8_1
+*/
+
 int charToIndex(char c) {
     return (c - 'A');
 }
