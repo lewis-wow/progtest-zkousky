6e48ce947642bc9b31e1bef51b5adb0634363367
Ludvík Prokopec <ludvik.prokopec@gmail.com>
2022-01-04 14:41:40 +0100

    zadani mapa

---
+++ mapa/index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/*
+    MAPA
+    vstup:
+    [
+        [0, 1, 1, 1],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [1, 1, 0, 0]
+    ]
+    program: vyhledat ostrovy s cislem "1" a tyto ostrovy smazat (udelat z nich 0)
+    ostrov: misto s jednickami, ktere neni spojene s koncema mapy
+    mapa: muze byt jakkoliv velka, dulezite je aby byla na vstupu zadana jako 2d pole
+    teorie: rozdelit si mapu na ostrovy, detekovat hrany ostrovu, odstranit cele ostrovy, ktere hranu nemaji na okraji mapy
+    vystup:
+    [
+        [0, 1, 1, 1],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 0, 0]
+    ]
+*/
+
+int main(void) {
+
+    return 0;
+}
